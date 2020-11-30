9df3e58aad34fe9adb2b234ede1d4021b96053ce
tian0912 <tianbin0912@163.com>
2020-11-30 21:47:30 +0800

    实现像,退出功能

--- js/index.js
+++ js/index.js
@@ -3,5 +3,37 @@ $.ajax({
     url:'/my/userinfo',
     success:function(res){
         console.log(res);
+        getImg(res.data)
     }
 })
+//实现退出功能，退出跳转到登录界面，并且要删除本地缓存的请求头
+$('#btnLogout').on('click',function(){
+    layer.confirm('是否退出?', {icon: 3, title:'提示'}, function(index){
+        //清空本地缓存
+        localStorage.removeItem('token');
+        //跳转到登录界面
+        location.href='login.html'
+        
+        layer.close(index);
+      });
+    
+})
+//封装改变头像和名称的函数
+function getImg(user){
+    var name = user.nickname || user.username;
+    //拼接欢迎字符串
+    $('#huanying').html('欢迎&nbsp;&nbsp;'+name);
+
+    //渲染头像
+    if(user.user_pic !== null){
+        $('.text-img').hide();
+        $('.layui-nav-img').attr(
+            'src',user.user_pic
+        ).hide();
+    }else {
+        var first = name[0].toUpperCase();
+        $('.text-img').text(first).show();
+        $('.layui-nav-img').hide();
+    }
+
+}
